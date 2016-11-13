128c522d516a9bd77bb9d2501483d7b18d09709b
AspireR7 <AspireR7@ASPIRER7-PC>
2016-11-13 19:27:54 +0100

    AdminMetder for test ferdig skrevet.

--- DAL/NettbankDALStub.cs
+++ DAL/NettbankDALStub.cs
@@ -7,7 +7,8 @@ namespace Nettbanken.DAL
 {
     public class NettbankDALStub : INettbankDAL
     {
-
+        List<string[]> listeAvKunder;
+        List<byte[]> ph;
         // ---------------------------------------------------------------------------------------
         // Admin Metoder
 
@@ -45,29 +46,145 @@ namespace Nettbanken.DAL
             return allekunder;
         }
 
+        //kunder registrerers i minne..
         public bool registrerNyKunde(Kunde kunde)
         {
-            throw new NotImplementedException();
+            if (listeAvKunder==null) {
+                listeAvKunder = new List<string[]>();
+                ph = new List<byte[]>();
+            }
+            
+            string personNr, bankId, passord,fornavn, etternavn, addresse, telefonNr, postNr;
+            byte[] salt;
+
+            personNr = kunde.postNr;
+            bankId = kunde.bankId;
+            passord = kunde.passord;
+            salt = kunde.salt;
+            fornavn = kunde.fornavn;
+            etternavn = kunde.etternavn;
+            addresse = kunde.adresse;
+            telefonNr = kunde.telefonNr;
+            postNr = kunde.postNr;
+
+            string[] enKunde = { personNr,bankId,passord,fornavn,etternavn,addresse,telefonNr,postNr};
+
+            if (kunde == null)
+            {
+                return false;
+            }
+            else {
+                listeAvKunder.Add(enKunde);
+                ph.Add(salt);
+            }
+            return true;
         }
 
         public bool endreKunde(string idnr, Kunde innKunde)
         {
-            throw new NotImplementedException();
+            string[] kundenSomSkalEndres = null;
+
+            if (listeAvKunder != null)
+            {
+                for (int i = 0; i < listeAvKunder.Count; i++)
+                {
+                    string[] enKunde = listeAvKunder[i];
+                    if (enKunde[0].Equals(idnr))
+                    {
+                        kundenSomSkalEndres = enKunde;
+                        ph[i] = innKunde.salt;
+                        break;
+                    }
+                }
+                //innholdet til arrayen som inneholder kunden med gitt id oppdateres
+                kundenSomSkalEndres[0] = innKunde.personNr;
+                kundenSomSkalEndres[1] = innKunde.bankId;
+                kundenSomSkalEndres[2] = innKunde.passord;
+                kundenSomSkalEndres[3] = innKunde.fornavn;
+                kundenSomSkalEndres[4] = innKunde.etternavn;
+                kundenSomSkalEndres[5] = innKunde.adresse;
+                kundenSomSkalEndres[6] = innKunde.telefonNr;
+                kundenSomSkalEndres[7] = innKunde.postNr;
+                return true;
+            }
+            else {
+                return false;
+            }
+            
         }
 
+        //slett en kunde
         public bool slettKunde(string personNr)
         {
-            throw new NotImplementedException();
+            string[] kundenSomSkalSlettes = null;
+
+            if (listeAvKunder != null)
+            {
+                for (int i = 0; i < listeAvKunder.Count; i++)
+                {
+                    string[] enKunde = listeAvKunder[i];
+                    if (enKunde[0].Equals(personNr))
+                    {
+                        kundenSomSkalSlettes = enKunde;
+                        ph.RemoveAt(i);
+                        break;
+                    }
+                }
+                listeAvKunder.Remove(kundenSomSkalSlettes);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public Kunde finnKunde(string sok)
         {
-            throw new NotImplementedException();
+            Kunde funnet = new Kunde();
+            string[] kundenFunnet = null;
+
+            if (listeAvKunder != null)
+            {
+                for (int i = 0; i < listeAvKunder.Count; i++)
+                {
+                    string[] enKunde = listeAvKunder[i];
+                    if (enKunde[0].Equals(sok))
+                    {
+                        kundenFunnet = enKunde;
+                        funnet.salt = ph[i];
+                        break;
+                    }
+                } 
+                funnet.personNr = kundenFunnet[0];
+                funnet.bankId = kundenFunnet[1];
+                funnet.passord = kundenFunnet[2];
+                funnet.personNr = kundenFunnet[3];
+                funnet.personNr = kundenFunnet[4];
+                funnet.personNr = kundenFunnet[5];
+                funnet.personNr = kundenFunnet[6];
+                funnet.personNr = kundenFunnet[7];
+                return funnet;
+            }
+            else
+            {
+                return null; 
+            }
+
         }
 
         public String lagPassord()
         {
-            throw new NotImplementedException();
+            string velgFra = "abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789!@$?_-";
+            char[] bokstaver = new char[9];
+            Random tilfeldig = new Random();
+
+            for (int i = 0; i < 9; i++)
+            {
+                bokstaver[i] = velgFra[tilfeldig.Next(0, velgFra.Length)];
+            }
+
+            return new string(bokstaver);
         }
 
         // ---------------------------------------------------------------------------------------
